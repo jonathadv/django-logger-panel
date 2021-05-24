f207e1edee2076a2c0fc9648f950eb79aefad8c7
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-05-23 22:29:19 -0300

    Adds pylint and black

--- setup.py
+++ setup.py
@@ -50,6 +50,52 @@ else:
     about['__version__'] = VERSION
 
 
+class LintCommand(Command):
+    """Run pylint"""
+
+    description = "Run pylint."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status("Running Pylint...")
+        os.system(f"pipenv run pylint {MODULE_NAMES}".format(sys.executable))
+        sys.exit()
+
+
+class FormatCommand(Command):
+    """Format code with Black"""
+
+    description = "Format code with Black."
+    user_options = []
+
+    @staticmethod
+    def status(s):
+        """Prints things in bold."""
+        print("\033[1m{0}\033[0m".format(s))
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        self.status("Formatting...")
+        os.system("pipenv run black .".format(sys.executable))
+        sys.exit()
+
+
 class UploadCommand(Command):
     """Support setup.py upload."""
 
@@ -118,5 +164,7 @@ setup(
     # $ setup.py publish support.
     cmdclass={
         'upload': UploadCommand,
+        "format": FormatCommand,
+        "lint": LintCommand,
     },
 )
