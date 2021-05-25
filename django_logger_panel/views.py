97f3b3fab379b8a36c6532700ebc98dca7e5e34c
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-05-24 22:23:01 -0300

    Moves common components to base.html

--- django_logger_panel/views.py
+++ django_logger_panel/views.py
@@ -1,18 +1,14 @@
 import logging
+
 from django import forms
 from django.core.exceptions import ValidationError
 from django.views.generic import TemplateView
-from django.views.generic.edit import FormView
 
 from . import BASE_URL
 from .__version__ import __version__
 from .loggerpanel import LEVELS, set_logger_level, get_all_loggers, get_all_log_level
 
 LOGGER = logging.getLogger(__name__)
-BASE_CONTEXT = {
-    "base_url": BASE_URL,
-    "project_version": __version__
-}
 
 
 class SetLoggerLevelForm(forms.Form):
@@ -21,10 +17,26 @@ class SetLoggerLevelForm(forms.Form):
 
 
 class LoggerBaseView(TemplateView):
+    _first_crumb = {"url": BASE_URL, "name": "Loggers", "is_last": True}
+
     def get_context_data(self, **kwargs):
+        base_context = {
+            "base_url": BASE_URL,
+            "project_version": __version__,
+            "breadcrumb": self.get_base_breadcrumb()
+        }
         context: dict = super().get_context_data(**kwargs)
-        context.update(BASE_CONTEXT)
+        context.update(base_context)
         return context
+
+    def set_breadcrumb(self, context: dict, url: str, name: str):
+        crumb = {"url": url, "name": name, "is_last": True}
+        context["breadcrumb"] = self.get_base_breadcrumb()
+        context["breadcrumb"][0]["is_last"] = False
+        context["breadcrumb"].append(crumb)
+
+    def get_base_breadcrumb(self):
+        return [dict(self._first_crumb)]
 
 
 class LoggerListView(LoggerBaseView):
@@ -60,7 +72,6 @@ class LoggerListView(LoggerBaseView):
 
     def get_context_data(self, **kwargs):
         context: dict = super().get_context_data(**kwargs)
-        context.update(BASE_CONTEXT)
         context["loggerdict"] = get_all_loggers()
         context["all_log_level"] = get_all_log_level()
         context["log_levels"] = LEVELS
@@ -72,7 +83,7 @@ class LoggingDetailView(LoggerBaseView):
 
     def get_context_data(self, log_name=None, **kwargs):
         context: dict = super().get_context_data(**kwargs)
-        context.update(BASE_CONTEXT)
+        self.set_breadcrumb(context, log_name, log_name)
         loggers = get_all_loggers()
         context["log_name"] = log_name
         context["logger"] = loggers.get(log_name)
