e911d3b8f24856b6471ef77eca44ab5224367617
Jonatha Daguerre Vasconcelos <jonatha@daguerre.com.br>
2021-05-24 23:50:53 -0300

    Improves the detail view

--- django_logger_panel/templatetags/loggerpanel.py
+++ django_logger_panel/templatetags/loggerpanel.py
@@ -9,3 +9,10 @@ register = template.Library()
 def log_level_name(value):
     """Removes all values of arg from the given string"""
     return getLevelName(value)
+
+@register.filter(name="class_name")
+def class_name(class_instance):
+    """Removes all values of arg from the given string"""
+    if class_instance:
+        return class_instance.__class__.__name__
+    return ""
